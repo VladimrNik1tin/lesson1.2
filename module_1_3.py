9ba078fe16f176318a57599a4751993096e76f35
VladimrNik1tin <nikitin.vladimir99@mail.ru>
2024-09-21 18:32:57 +0400

    Update and rename Variables.py to module_1_3.py

---
+++ module_1_3.py
@@ -0,0 +1,5 @@
+the_number_of_completed_DZ = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent/the_number_of_completed_DZ
+print('Курс:',course_name + ",",'всего задач:'+str(the_number_of_completed_DZ) + ",",'затрачено часов:'+str(number_of_hours_spent)+",",'среднее время выполнения',time_for_one_task,'часа.')
